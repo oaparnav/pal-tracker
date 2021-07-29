2313db1bab696fb732ecf57e9bfe78cf7c923aa7
Ubuntu <ubuntu@ip-172-31-37-6.ap-southeast-1.compute.internal>
2021-07-29 07:14:57 +0000

    Implement new jdbc repository

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,12 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+    @Bean
+    public MysqlDataSource getMySqlDataource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(dataSourceUrl);
+        return dataSource;
     }
 }
